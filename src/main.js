dcfc4040779c39e496e0e33210e278deaf5c261c
Bimo Prakoso <biimoprakosoo@gmail.com>
2023-06-29 19:25:19 +0700

    Adding function to make the conversation on new channel

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 const dotenv = require('dotenv');
-const { Client, IntentsBitField} = require('discord.js');
+const { Client, GatewayIntentBits, ChannelType } = require('discord.js');
 const { Configuration, OpenAIApi } = require('openai');
 const path = require("path");
 
@@ -7,9 +7,9 @@ dotenv.config({ path: path.join(__dirname, '../.env') });
 
 const client = new Client({
   intents: [
-    IntentsBitField.Flags.Guilds,
-    IntentsBitField.Flags.GuildMessages,
-    IntentsBitField.Flags.MessageContent,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
   ]
 });
 
@@ -28,29 +28,58 @@ client.on('messageCreate', async (message) => {
 
   let conversationLog = [{ role: 'system', content: "You are a friendly chat-bot." }];
 
-  await message.channel.sendTyping();
+  if(message.mentions.has(client.user)) {
+    await message.channel.sendTyping();
 
-  let prevMessages = await message.channel.messages.fetch({ limit: 15});
-  prevMessages.reverse();
+    const threadManager = message.guild.channels.cache.find(channel => channel.type === ChannelType.GuildCategory && channel.name === 'Threads');
 
-  prevMessages.forEach((msg) => {
-    if (message.content.startsWith('!')) return;
-    if (msg.author.id !== client.user.id && message.author.bot) return;
-    if (msg.author.id !== message.author.id) return;
+    if (!threadManager) {
+      console.log('Threads category not found');
+      return;
+    }
+
+    const threadChannel = await message.channel.guild.channels.create({
+      name: "Chat Thread",
+      type: ChannelType.GuildText,
+      parent: process.env.CATEGORY_CHANNEL, // Replace this with the actual ID of the your category channel
+      reason: 'New chat thread created.',
+    });
 
     conversationLog.push({
       role: 'user',
-      content: msg.content,
-    })
-  })
+      content: message.content
+    });
 
-  const result = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo',
-    messages: conversationLog,
-  });
-
-  const botReply = result.data.choices[0].message.content;
-  message.reply(botReply);
+    const threadReply = await executeChat(message, threadChannel, conversationLog);
+    message.reply(threadReply);
+  }
 });
+
+const executeChat = async (message, threadChannel, conversationLog) => {
+  while (true) {
+    const result = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: conversationLog,
+    });
+
+    const botReply = result.data.choices[0].message.content;
+
+    await threadChannel.send(botReply);
+
+    const collectedMessages = await threadChannel.awaitMessages({ limit:1, time: 300000, errors: ['time']});
+    const userMessage = collectedMessages.first().content;
+
+    if (userMessage.toLowerCase() === 'stop') {
+      await threadChannel.send('Chat session ended. Thread will be archived');
+      threadChannel.setArchived(true, 'End of chat session.');
+      break;
+    }
+
+    conversationLog.push({role: 'user', content: userMessage});
+    conversationLog.push({role: 'assistant', content: botReply});
+  }
+
+  return 'Chat session ended';
+}
 
 client.login(process.env.DISCORD_TOKEN); //change your discord token in .env
